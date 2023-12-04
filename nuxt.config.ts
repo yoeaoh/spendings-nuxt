229230f018f6a14fbf7b70ae19eb66d0719fcd25
yoeaoh <yoeaoh@gmail.com>
2023-12-04 23:06:50 +0300

    moved to src/ folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,24 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-    devtools: { enabled: false },
-    css: ['~/assets/scss/global.scss'],
-    modules: ['@pinia/nuxt', 'nuxt-icon', '@nuxtjs/google-fonts'],
-    googleFonts: {
-        prefetch: true,
-        preconnect: false,
-        preload: true,
-        useStylesheet: true,
-        families: {
-            Roboto: true,
-            Oswald: [300, 400, 500],
-            Raleway: [300, 400, 500, 600, 700, 800],
-            Inter: [300, 400, 500, 600, 700, 800],
+import { resolvePath } from '@nuxt/kit';
+
+export default async () =>
+    defineNuxtConfig({
+        devtools: { enabled: false },
+        css: ['~/assets/scss/global.scss'],
+        alias: {
+            '~/': await resolvePath('./src/'),
         },
-    },
-});
+        srcDir: 'src/',
+        modules: ['@pinia/nuxt', 'nuxt-icon', '@nuxtjs/google-fonts'],
+        googleFonts: {
+            prefetch: true,
+            preconnect: false,
+            preload: true,
+            useStylesheet: true,
+            families: {
+                Roboto: true,
+                Oswald: [300, 400, 500],
+                Raleway: [300, 400, 500, 600, 700, 800],
+                Inter: [300, 400, 500, 600, 700, 800],
+            },
+        },
+    });
